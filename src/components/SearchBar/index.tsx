12ac358f18c017272ca178ab8644243160b3617a
Jonathan Ledlow <jledlow@adagetech.com>
2024-08-23 16:51:02 -0500

    updates to searchbar

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,8 +1,9 @@
 // components/SearchBar.tsx
 'use client';
-import React, {dangerouslySetInnerHTML} from 'react';
+import React, {dangerouslySetInnerHTML, useEffect} from 'react';
 import { searchArticles } from '../cms/page/SearchPage/api';
-import { ArticleList } from '../cms/page/ArticleGroupPage/api';
+import { ArticleList, ArticleListItem } from '../cms/page/ArticleGroupPage/api';
+import { getSummary } from '@/openaiQuery';
 
 interface SearchBarProps {
   doSearch: (searchTerm: string) => Promise<ArticleList>;
@@ -12,7 +13,8 @@ interface SearchBarProps {
 
 const SearchBar: React.FC<SearchBarProps> = ({ doSearch }) => {
   const [searchTerm, setSearchTerm] = React.useState('');
-  const [searchResults, setSearchResults] = React.useState([]);
+  const [searchResults, setSearchResults] = React.useState(null as ArticleListItem[] | null);
+  const [summary, setSummary] = React.useState('');
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
@@ -22,6 +24,15 @@ const SearchBar: React.FC<SearchBarProps> = ({ doSearch }) => {
     setSearchResults(results.items);
   })};
 
+  useEffect(() => {
+    if (!searchResults) return;
+    var topResults = searchResults.slice(0, 5);
+    getSummary(topResults.map((result) => result.intro?.toString() as string)).then((summary) => {
+      setSummary(summary);
+    });
+  },
+  [searchResults]);
+
   return (
     <div style={{maxWidth: '700px'}}>
       <input
@@ -30,6 +41,7 @@ const SearchBar: React.FC<SearchBarProps> = ({ doSearch }) => {
         placeholder="Search..."
       />
       <button onClick={handleSearch}>Search</button>
+      {summary && <div dangerouslySetInnerHTML={{__html: summary}} />}
       <ul>
         {searchResults && searchResults.map((result) => (
           <li key={result.key} style={{border: '1px solid black', marginBottom: '10px', padding: '10px'}}>
